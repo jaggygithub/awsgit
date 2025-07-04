4fce7b21b6fc967669aa4dfd26f8e52b2e3123a3
root <root@ip-172-31-35-145.eu-north-1.compute.internal>
2025-07-04 16:13:16 +0000

    sample python code

---
+++ sample.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+print('Add ',add(10,20));
+print('Sub ',sub(10,20));
+print('Mul ',mul(10,20));
+print('Div ',div(10,20));
