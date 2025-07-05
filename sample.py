5e93b2a8745f33986a09fda8d440c282f5d3b072
jaggygithub <143251682+jaggygithub@users.noreply.github.com>
2025-07-05 13:24:21 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,7 +10,11 @@ def mul(x,y):
 def div(x,y):
     return x/y
 
+def mod(x,y):
+    return x%y
+
 print('Add ',add(10,20));
 print('Sub ',sub(10,20));
 print('Mul ',mul(10,20));
 print('Div ',div(10,20));
+print('Mod ',mod(25,3));
